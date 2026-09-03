cfb23cebee9c131cd3aa5a7c71469ec71b262730
gustavoverneck <gustavoverneck@gmail.com>
2026-09-03 12:46:30 -0300

    Configura publicacao no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
+  base: '/corretor-simulado/',
   plugins: [react()],
   server: { host: '0.0.0.0', port: 4173 },
 })
